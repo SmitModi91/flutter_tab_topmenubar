fe4b85ad6a2db5c3de8f669ee82aa30ce0435c76
smitmodi <modismit2@gmail.com>
2023-02-16 20:41:06 +0530

    Added top bar menu example

---
+++ lib/data_transfer_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DataTransferPageStarter extends StatelessWidget {
+  const DataTransferPageStarter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+        child: Text("Data  Transfer"),
+      color: Colors.black,
+    );
+  }
+}
---
+++ lib/performance_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PerformancePage extends StatelessWidget {
+  const PerformancePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
